5d1f0f7bbd410bc233ecf1a96f9661a00e50708f
mengxianling <xl.meng@mobiletek.cn>
2018-09-13 11:00:32 +0800

    test again in other git kuu

---
+++ README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+# gitskill
+=======
+# learngit
+###git 学习
+>>>>>>> Create README.md
---
+++ app/src/main/java/com/example/administrator/opencvcamera/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.administrator.opencvcamera;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.YuvImage;
+import android.hardware.Camera;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener {
+    JavaCameraView openCvCameraView;
+    private CascadeClassifier cascadeClassifier;
+    private Mat grayscaleImage;
+    private int absoluteFaceSize;
+
+    private void initializeOpenCVDependencies() {
+        try {
+            // Copy the resource into a temp file so OpenCV can load it
+            InputStream is = getResources().openRawResource(R.raw.lbpcascade_frontalface);
+            File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+            File mCascadeFile = new File(cascadeDir, "lbpcascade_frontalface.xml");
+            FileOutputStream os = new FileOutputStream(mCascadeFile);
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            is.close();
+            os.close();
+            // Load the cascade classifier
+            cascadeClassifier = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+        } catch (Exception e) {
+            Log.e("OpenCVActivity", "Error loading cascade", e);
+        }
+        // And we are ready to go
+        openCvCameraView.enableView();
+        openCvCameraView.setCameraIndex(1);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        openCvCameraView = (JavaCameraView) findViewById(R.id.jcv);
+        openCvCameraView.setCameraIndex(-1);
+        openCvCameraView.setCvCameraViewListener(this);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.e("log_wons", "OpenCV init error");
+        }
+        initializeOpenCVDependencies();
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        grayscaleImage = new Mat(height, width, CvType.CV_8UC4);
+
+
+        // The faces will be a 20% of the height of the screen
+        absoluteFaceSize = (int) (height * 0.2);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+    }
+    int faceSerialCount = 0;
+    @Override
+    public Mat onCameraFrame(Mat aInputFrame) {
+        Imgproc.cvtColor(aInputFrame, grayscaleImage, Imgproc.COLOR_RGBA2RGB);
+        MatOfRect faces = new MatOfRect();
+        if (cascadeClassifier != null) {
+            cascadeClassifier.detectMultiScale(grayscaleImage, faces, 1.1, 2, 2,
+                    new Size(absoluteFaceSize, absoluteFaceSize), new Size());
+        }
+        Rect[] facesArray = faces.toArray();
+        int faceCount = facesArray.length;
+        if (faceCount > 0) {
+            faceSerialCount++;
+        } else {
+            faceSerialCount = 0;
+        }
+        if (faceSerialCount > 6) {
+            openCvCameraView.takePhoto("sdcard/aaa.jpg");
+            faceSerialCount = -5000;
+        }
+        for (int i = 0; i < facesArray.length; i++) {
+            Imgproc.rectangle(aInputFrame, facesArray[i].tl(), facesArray[i].br(), new Scalar(0, 255, 0, 255), 3);
+        }
+        return aInputFrame;
+    }
+}
